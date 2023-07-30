415536cf02422ea5f2a723e2cab0d7698acace91
Ay-slim <ayooluwaadedipe@gmail.com>
2023-07-30 23:09:05 +0000

    Use docker to deploy puppeteer

--- src/endpoints/profile_details.ts
+++ src/endpoints/profile_details.ts
@@ -15,6 +15,7 @@ export const scrape_profile_details = async (req: RequestUsername, res: Response
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     headless: false,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
   });
   const page = await browser.newPage();
   await page.goto("https://www.instagram.com/accounts/login", {
--- src/scraper_logic/comments.ts
+++ src/scraper_logic/comments.ts
@@ -26,6 +26,7 @@ export const scrape_comments = async(athlete_id: number, batch_id: number, usern
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     headless: false,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
   });
   const page = await browser.newPage();
   await page.goto("https://www.instagram.com/accounts/login", {
